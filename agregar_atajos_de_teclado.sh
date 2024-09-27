f1e6ff3b3c8a944bb4391812e02b2185250eff34
Enmanuel Rodriguez <143436386+enma11235@users.noreply.github.com>
2024-09-27 15:10:34 -0300

    Update and rename atajoTeclado.sh to agregar_atajos_de_teclado.sh

--- agregar_atajos_de_teclado.sh
+++ agregar_atajos_de_teclado.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# agregamos la el atajo CTRL + ALT + T para abir la terminal
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/ name 'Open Terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/ command 'gnome-terminal'
