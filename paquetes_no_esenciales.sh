0a8a92e17fa8d2dbbc98607ab63c9963dcdd7022
Enmanuel Rodriguez <143436386+enma11235@users.noreply.github.com>
2024-09-27 18:51:49 -0300

    Update and rename remover_paquetes to paquetes_no_esenciales.sh

--- paquetes_no_esenciales.sh
+++ paquetes_no_esenciales.sh
@@ -14,3 +14,29 @@ libreoffice-script-provider-bsh libreoffice-script-provider-js \
 libreoffice-script-provider-python libreoffice-sdbc-firebird libreoffice-sdbc-hsqldb \ 
 libreoffice-sdbc-mysql libreoffice-sdbc-postgresql libreoffice-style-colibre \
 libreoffice-style-elementary libreoffice-writer
+
+7zip
+accountsservice
+acl
+adduser
+adwaita-icon-theme
+alsa-topology-conf
+alsa-ucm-conf
+alsa-utils
+amd64-microcode
+anacron
+ant
+ant-optional
+apache2-bin
+apg
+apparmor
+appstream
+aspell
+aspell-en
+at-spi2-common
+at-spi2-core
+atmel-firmware
+atril
+atril-common
+avahi-daemon
+
