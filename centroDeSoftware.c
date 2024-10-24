952b685f6d01415d6b9dd809ca92a89336995428
Enmanuel Rodriguez <143436386+enma11235@users.noreply.github.com>
2024-10-24 16:15:14 -0300

    Update centroDeSoftware.c

--- centroDeSoftware.c
+++ centroDeSoftware.c
@@ -1,37 +1,50 @@
+  GNU nano 8.2                                                                                         ventana.c                                                                                                   
 #include <gtk/gtk.h>
 
-static void on_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("¡Botón presionado!\n");
-}
-
-int main(int argc, char *argv[]) {
-    GtkWidget *window;
-    GtkWidget *button;
-
-    gtk_init(&argc, &argv);
-
+static void on_activate(GtkApplication *app, gpointer user_data) {
     // Crear una nueva ventana
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Aplicación GTK");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Ventana con Panel Lateral");
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 400);
 
-    // Crear un botón
-    button = gtk_button_new_with_label("Presiona aquí");
+    // Crear un contenedor de tipo GtkBox horizontal
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_window_set_child(GTK_WINDOW(window), box);
 
-    // Conectar la señal del botón a la función de callback
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    // Crear un panel lateral izquierdo (GtkBox vertical)
+    GtkWidget *sidebar = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
-    // Conectar la señal para cerrar la ventana
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    // Agregar algunos botones al panel lateral
+    GtkWidget *button1 = gtk_button_new_with_label("Centro de software");
+    GtkWidget *button2 = gtk_button_new_with_label("Aplicaciones");
+        GtkWidget *button3 = gtk_button_new_with_label("Aplicaciones Instaladas");
+        GtkWidget *button4 = gtk_button_new_with_label("Sistema");
+    gtk_box_append(GTK_BOX(sidebar), button1);
+    gtk_box_append(GTK_BOX(sidebar), button2);
+        gtk_box_append(GTK_BOX(sidebar), button3);
+        gtk_box_append(GTK_BOX(sidebar), button4);
 
-    // Agregar el botón a la ventana
-    gtk_container_add(GTK_CONTAINER(window), button);
+    // Crear un área de contenido principal
+    GtkWidget *content_area = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    GtkWidget *label = gtk_label_new("Contenido central");
+    gtk_box_append(GTK_BOX(content_area), label);
 
-    // Mostrar todo
-    gtk_widget_show_all(window);
+    // Agregar el panel lateral y el área de contenido al contenedor principal
+    gtk_box_append(GTK_BOX(box), sidebar);
+    gtk_box_append(GTK_BOX(box), content_area);
 
-    // Iniciar el bucle principal de GTK
-    gtk_main();
-
-    return 0;
+    // Mostrar todos los widgets
+    gtk_widget_show(window);
 }
+
+int main(int argc, char **argv) {
+    GtkApplication *app = gtk_application_new("com.example.gtk4", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return status;
+}
+
+
+
+
